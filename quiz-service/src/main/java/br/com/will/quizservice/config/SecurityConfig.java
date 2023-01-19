f275468c6ac58e7bec525a431912088ec4aa8bfe
will <william.alves@platformbuilders.io>
2023-01-19 02:49:25 -0300

    now swagger-ui open

--- quiz-service/src/main/java/br/com/will/quizservice/config/SecurityConfig.java
+++ quiz-service/src/main/java/br/com/will/quizservice/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package br.com.will.quizservice.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -14,7 +13,20 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
    protected void configure(HttpSecurity http) throws Exception {
-      http.authorizeRequests(authorize -> authorize.anyRequest().permitAll())
-            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    http.authorizeRequests(
+            authorize -> {
+              try {
+                authorize
+                    .antMatchers("/swagger-ui/**", "/v3/api-docs/**")
+                    .permitAll()
+                    .and()
+                    .authorizeRequests()
+                    .anyRequest()
+                    .authenticated();
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+            })
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
    }
 }
