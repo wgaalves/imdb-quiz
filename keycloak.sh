4cbba4bf0920f8f02e246a0a44ac2d2adbb138ed
will <william.alves@platformbuilders.io>
2023-01-19 22:45:30 -0300

    readme.md  changes

--- keycloak.sh
+++ keycloak.sh
@@ -5,5 +5,5 @@ sleep 60
 docker exec -it keycloak /opt/jboss/keycloak/bin/kcadm.sh config credentials –server http://localhost:8888/auth –realm master –user spring –password spring123 && \
 docker exec -it keycloak /opt/jboss/keycloak/bin/kcadm.sh create -x “client-scopes” -r master -s name=TEST -s protocol=openid-connect && \
 docker exec -it keycloak /opt/jboss/keycloak/bin/kcadm.sh create clients -r master -s clientId=spring-without-test-scope -s enabled=true -s clientAuthenticatorType=client-secret -s secret=79a93cb3-b460-40c8-8c96-c8c8bfe47d39 -s ‘redirectUris=[“*”]’ && \
-docker exec -it keycloak /opt/jboss/keycloak/bin/kcadm.sh create clients -r master -s clientId=spring-with-test-scope -s enabled=true -s clientAuthenticatorType=client-secret -s secret=b129f0c2-a46a-4bdb-a059-4eca03639767 -s ‘redirectUris=[“*”]’ -s ‘defaultClientScopes=[“TEST”, “web-origins”, “profile”, “roles”, “email”]’
+docker exec -it keycloak /opt/jboss/keycloak/bin/kcadm.sh create clients -r master -s clientId=spring-with-test-scope -s enabled=true -s clientAuthenticatorType=client-secret -s secret=b129f0c2-a46a-4bdb-a059-4eca03639767 -s ‘redirectUris=[“*”]’ -s ‘defaultClientScopes=[“QUIZ_PLAY_ROLE”, “web-origins”, “profile”, “roles”, “email”]’
 
--- readme.md
+++ readme.md
@@ -1,4 +1,47 @@
 
-### 
-#### docker build ./gateway/ -t quiz/gateway -f gateway/Dockerfile
-#### docker build ./quiz-service/ -t quiz/quiz-service -f quiz-service/Dockerfile
+# [Imdb Quiz with oauth2 authentication](https://github.com/wgaalves/imdb-quiz/)
+
+## What you'll build
+- A simple spring cloud project with oath2 authentication
+
+## What you'll need
+- Docker CE
+
+## Stack
+- Docker
+- Docker-compose
+- Java 11/77
+- Spring Boot
+- Keycloak
+- Gradle
+
+## Befor Run
+- Run command `docker build ./gateway/ -t quiz/gateway -f gateway/Dockerfile`
+- Run command `docker build ./quiz-service/ -t quiz/quiz-service -f quiz-service/Dockerfile`
+
+## Run
+- Run command `docker-compose up -d`
+
+## Script After Run
+
+-
+
+## Documentation
+- Swagger on `http://localhost:8060/quiz/swagger-ui/index.html`
+
+
+## Sequence Diagram
+
+```mermaid
+sequenceDiagram
+User->>Gateway: /login
+Gateway->>Keycloak: /authenticate
+Keycloak->>Gateway: jwt
+Gateway->>User: jwt
+User->>Gateway: /quiz/**
+Gateway->>Quiz-Service: /quiz/**
+Quiz-Service->>Keycloak: verify access_key
+Keycloak->>Quiz-Service: response
+Quiz-Service->>Gateway: resource response
+Gateway->>User: resource response
+```
